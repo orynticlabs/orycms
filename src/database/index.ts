a27f6bc4e22cba0ff44b616f95214fc3125855c6
Tushar Gupta <tushargupta@192.168.2.119>
2026-07-10 15:22:32 +0530

    Updated

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,11 @@
 export type * from "./adapter.types";
 export type { OryCMSDatabaseAdapter } from "./adapter.interface";
 
-export { OryCMSPostgreSQLAdapter } from "./adapters/postgresql.adapter";
+export {
+  OryCMSPostgreSQLAdapter,
+  createOryCMSPostgreSQLAdapter,
+  createMigrationFromCollectionPlan,
+} from "./adapters/postgresql.adapter";
 export { OryCMSMySQLAdapter } from "./adapters/mysql.adapter";
 export { OryCMSMongoDBAdapter } from "./adapters/mongodb.adapter";
 export { OryCMSFirebaseAdapter } from "./adapters/firebase.adapter";
