140df31d4771e488a1a937a69307a8270b9481da
Tushar Gupta <tushargupta@192.168.2.119>
2026-07-10 15:39:29 +0530

    5-create-only-the-orycms-core-system-schema-and-migration-plan

---
+++ src/core/index.ts
@@ -0,0 +1,3 @@
+export { getOryCMSCoreCollections } from "./core.collections";
+export { generateOryCMSCoreMigrationPlan, installOryCMSCoreSchema } from "./core.migration";
+export type { OryCMSCoreInstallResult } from "./core.migration";
